c096fa7cd4e6065c72720a9ad7bb72b014882a9c
Esteban Molina Mejia <emolinam@eafit.edu.co>
2025-11-05 06:37:45 -0500

    docker

--- Tickio_project/tickio/urls.py
+++ Tickio_project/tickio/urls.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
+from django.http import JsonResponse
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.i18n import set_language
 
+def health_check(request):
+    """Health check endpoint for Docker/load balancers"""
+    return JsonResponse({"status": "healthy"})
+
 urlpatterns = [
+    path("health/", health_check, name="health_check"),
     path("admin/", admin.site.urls),
     path("i18n/setlang/", set_language, name="set_language"),
     path("", include("events.urls")),
